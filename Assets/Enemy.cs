fed4dabae5cc5a8ae3b96753f8b63413f045fa4c
LudumJammer <johcarlberg@hotmail.com>
2015-11-22 16:35:33 +0100

    Added straight-moving enemy and a spawner that spawns enemies on a plane

---
+++ Assets/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	
+	public enum MovementPattern {
+		Straight
+	}
+
+	public float movementSpeed = 1.0f;
+	public MovementPattern movementPattern;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (movementPattern == MovementPattern.Straight)
+		{
+			transform.position = transform.position + new Vector3(0, 0, -movementSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public Transform[]  spawnList;
+	public float spawnInterval = 1.0f;
+	public float width = 20.0f;
+	public float height = 10.0f;
+
+	private float timer = 0.0f;
+	private int index = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= spawnInterval)
+		{
+			timer = 0.0f;
+			GameObject.Instantiate(spawnList[index], transform.position + RandomPlane(width, height), Quaternion.identity);
+			index++;
+			if (index >= spawnList.Length)
+			{
+				index = 0;
+			}
+		}
+	}
+
+	Vector3 RandomPlane(float width, float height)
+	{
+		return new Vector3((Random.value - 0.5f) * width, Random.value * height, 0.0f);
+	}
+}
