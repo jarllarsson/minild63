8dcfc251e7706d14e13906b095b874502b872a57
LudumJammer <jcarlberg@hotmail.com>
2015-11-22 15:35:28 +0100

    Script that scrolls a texture on a Quad. Added a Quad to the main scene.

---
+++ Assets/Scripts/ScrollingTexture.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingTexture : MonoBehaviour {
+
+	public float scrollSpeed = 0.1f;
+	public Material materialToScroll;
+
+	private float currentScroll = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentScroll += Time.deltaTime * scrollSpeed;
+		materialToScroll.SetTextureOffset("_MainTex", new Vector2(0.0f, currentScroll));
+	}
+
+	void OnDestroy () {
+		materialToScroll.SetTextureOffset("_MainTex", new Vector2(0.0f, 0.0f));
+	}
+}
