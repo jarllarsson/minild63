a1447672bf662f094d503f3432817f2d09af64fb
Jarl Larsson <jarl.larsson@gmail.com>
2015-11-22 20:26:16 +0100

    ship it

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIHandler : MonoBehaviour {
+    public Text m_player0Text, m_player1Text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_player0Text.text = "Player 1: " + ScoreManager.playerScore0;
+        m_player1Text.text = "Player 2: " + ScoreManager.playerScore1;
+	}
+}
