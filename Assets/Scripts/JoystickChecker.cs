3444a3386e282458398110b7397c15f98a3eccc4
Jarl Larsson <jarl.larsson@gmail.com>
2015-11-22 17:07:27 +0100

    input/loginout

---
+++ Assets/Scripts/JoystickChecker.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoystickChecker : MonoBehaviour 
+{
+    public float joystickCheckerTime = 1.0f;
+    private float joystickCheckerTimer = 0.0f;
+
+    private string[] joysticks;
+
+    public PlayerScript secondPlayerToHideWhenNoController;
+
+	// Use this for initialization
+	void Start () {
+        updateList();   
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        joystickCheckerTimer += Time.deltaTime;
+        if (joystickCheckerTimer>joystickCheckerTime)
+        {
+            joystickCheckerTimer = 0.0f;
+            updateList();
+            if (joysticks.Length>1 && joysticks[1].Length > 0)
+            {
+                if (!secondPlayerToHideWhenNoController.isLoggedIn())
+                {
+                    secondPlayerToHideWhenNoController.Login();
+                }
+            }
+            else
+            {
+                if (secondPlayerToHideWhenNoController.isLoggedIn())
+                {
+                    secondPlayerToHideWhenNoController.Logout();
+                }
+                
+            }
+        }
+	}
+
+    void updateList()
+    {
+        joysticks = null;
+        joysticks = Input.GetJoystickNames();
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,11 +5,14 @@ public class PlayerScript : MonoBehaviour
 {
     public int m_playerNumber = 0;
     public float m_speed = 50.0f;
+    private bool m_loggedIn = true;
+
+    private Renderer m_renderer;
 
 	// Use this for initialization
 	void Start ()
     {
-	
+        m_renderer = transform.GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
@@ -18,4 +21,21 @@ public class PlayerScript : MonoBehaviour
         Vector2 inputMovement = new Vector2(Input.GetAxis("Horizontal" + m_playerNumber), Input.GetAxis("Vertical" + m_playerNumber));
         transform.localPosition += new Vector3(inputMovement.x, inputMovement.y, 0.0f) * Time.deltaTime * m_speed;
 	}
+
+    public void Logout()
+    {
+        m_renderer.enabled = false;
+        m_loggedIn = false;
+    }
+
+    public void Login()
+    {
+        m_renderer.enabled = true;
+        m_loggedIn = true;
+    }
+
+    public bool isLoggedIn()
+    {
+        return m_loggedIn;
+    }
 }
