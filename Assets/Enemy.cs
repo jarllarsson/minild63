9fcc35817fe7a524d82f46a7b6c545b6c187afc5
LudumJammer <johcarlberg@hotmail.com>
2015-11-22 18:46:13 +0100

    Collision between player and enemies. Enemy is destroyed and Player loses health.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -13,11 +13,24 @@ public class Enemy : MonoBehaviour {
 
 	public float movementSpeed = 1.0f;
 	public MovementPattern movementPattern;
+	public float radius = 0.5f;
+	public int damageToPlayer = 1;
 
 	private readonly float zDeathLimit = -15.0f;
 
+	private PlayerScript[] playerReferences;
+
 	// Use this for initialization
 	void Start () {
+		playerReferences = new PlayerScript[2];
+		int index = 0;
+		foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Player"))
+		{
+			if (index > 1)
+				break;
+			playerReferences[index] = obj.GetComponent<PlayerScript>();
+			index++;
+		}
 	}
 	
 	// Update is called once per frame
@@ -31,9 +44,23 @@ public class Enemy : MonoBehaviour {
 		{
 			Kill(KillReason.HitWall, 0);
 		}
+		
+		handlePlayerCollision(playerReferences[0], radius);
+		handlePlayerCollision(playerReferences[1], radius);
+	}
+	
+	private void handlePlayerCollision(PlayerScript player, float radius)
+	{
+		float squaredRadius = (player.m_radius + radius) * (player.m_radius + radius);
+		float squaredDistance = (player.transform.position - transform.position).sqrMagnitude;
+		if (squaredDistance < squaredRadius)
+		{
+			player.Damage(damageToPlayer);
+			Kill (KillReason.Player, 0);
+		}
 	}
 
-	public void Kill(KillReason reason, int value)
+	private void Kill(KillReason reason, int value)
 	{
 		Destroy(gameObject);
 	}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,6 +8,16 @@ public class PlayerScript : MonoBehaviour
     private bool m_loggedIn = true;
 
     private Renderer m_renderer;
+	
+	private enum PlayerState {
+		Normal,
+		Damaged
+	}
+	public float m_radius = 0.5f;
+	public float m_blinkTime = 1.5f;
+	public int m_health = 10;
+	private float m_blinkTimer = 0;
+	private PlayerState m_playerState;
 
 	// Use this for initialization
 	void Awake ()
@@ -19,7 +29,16 @@ public class PlayerScript : MonoBehaviour
 	void Update ()
     {
         Vector2 inputMovement = new Vector2(Input.GetAxis("Horizontal" + m_playerNumber), Input.GetAxis("Vertical" + m_playerNumber));
-        transform.localPosition += new Vector3(inputMovement.x, inputMovement.y, 0.0f) * Time.deltaTime * m_speed;
+		transform.localPosition += new Vector3(inputMovement.x, inputMovement.y, 0.0f) * Time.deltaTime * m_speed;
+		
+		if (m_playerState == PlayerState.Damaged)
+		{
+			m_blinkTimer += Time.deltaTime;
+			if (m_blinkTimer >= m_blinkTime)
+			{
+				m_playerState = PlayerState.Normal;
+			}
+		}
 	}
 
     public void Logout()
@@ -44,4 +63,13 @@ public class PlayerScript : MonoBehaviour
     {
         return m_loggedIn;
     }
+
+	public void Damage(int value)
+	{
+		if (m_playerState == PlayerState.Damaged)
+		{
+			m_health -= value;
+		}
+		m_playerState = PlayerState.Damaged;
+	}
 }
